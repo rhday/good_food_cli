b73f1d3cc0c6c2fb141571f636b18fd6735fee94
rhday <ruariday@gmail.com>
2020-04-15 14:21:00 +0100

    Escape

---
+++ lib/good_food_cli.rb
@@ -0,0 +1,6 @@
+require "good_food_cli/version"
+
+module GoodFoodCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/good_food_cli/version.rb
@@ -0,0 +1,3 @@
+module GoodFoodCli
+  VERSION = "0.1.0"
+end
