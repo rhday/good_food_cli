584cc20f982d4b67205ef30c3b50eb47df88eefc
rhday <ruariday@gmail.com>
2020-04-20 13:01:14 +0100

    cli show ingredients

--- lib/good_food_cli/cli.rb
+++ lib/good_food_cli/cli.rb
@@ -1,10 +1,13 @@
 class GoodFoodCli::Cli
     def call
-        puts 'Welcome to Good Food. Please browse our vegetarian recipes!'
+        puts "\nWelcome to Good Food. Here are all of our vegetarian recipes:\n"
         #binding.pry
         get_recipes
         list_recipes
+        get_user_recipe
         #get_method_for(recipe)
+        #show_ingredients_for
+        #show_method_for
     end
 
     def get_recipes
@@ -14,9 +17,27 @@ class GoodFoodCli::Cli
 
     def list_recipes
         #list recipes
+        
         @recipes.each.with_index(1) do |recipe, index|
             puts "#{index}. #{recipe}"
         end 
+        puts "Please type the number of the recipe you like the sound of:"
+    end 
+
+    def get_user_recipe
+        chosen_recipe = gets.strip.to_i
+        show_ingredients_for(chosen_recipe) if valid_input?(chosen_recipe, @recipes)
+        #binding.pry
+    end 
+
+    def valid_input?(input, data)
+        input.to_i <= data.length && input.to_i > 0
+    end 
+
+    def show_ingredients_for(chosen_recipe)
+        recipe = @recipes[chosen_recipe - 1]
+        puts "Here are the ingredients for #{recipe}:"
+        binding.pry
     end 
 
 end 
